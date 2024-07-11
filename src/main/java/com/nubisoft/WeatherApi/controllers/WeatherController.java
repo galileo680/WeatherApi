380ce73b7d1bf0d41d6177f3f573937cd545e7fd
galileo680 <bartek.wicz@gmail.com>
2024-07-11 13:18:13 +0200

    WeatherController updated

--- src/main/java/com/nubisoft/WeatherApi/controllers/WeatherController.java
+++ src/main/java/com/nubisoft/WeatherApi/controllers/WeatherController.java
@@ -1,10 +1,12 @@
 package com.nubisoft.WeatherApi.controllers;
 
 import com.nubisoft.WeatherApi.dtos.WeatherResponseDTO;
+import com.nubisoft.WeatherApi.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,12 +20,10 @@ public class WeatherController {
     }
 
     @GetMapping("/realtime-weather")
-    public List<WeatherResponseDTO> getRealtimeWeather() {
-
-    }
+    public List<WeatherResponseDTO> getRealtimeWeather() { return weatherService.getRealtimeWeather(); }
 
     @GetMapping("/forecast-weather")
     public List<WeatherResponseDTO> getForecastWeather() {
-
+        return weatherService.getForecastWeather();
     }
 }
