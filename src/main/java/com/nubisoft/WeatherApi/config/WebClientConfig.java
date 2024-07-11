f2f3fa4ff8f26e38f9b17b40d03671e22c97ce44
galileo680 <bartek.wicz@gmail.com>
2024-07-11 11:42:42 +0200

    WebClientConfig added

---
+++ src/main/java/com/nubisoft/WeatherApi/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package com.nubisoft.WeatherApi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+}
